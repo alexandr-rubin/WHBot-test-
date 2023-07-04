da74965d0e0001b94ad7a3101a7957c28a1b7285
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-04 17:35:41 +0300

    try c

--- src/app.ts
+++ src/app.ts
@@ -16,30 +16,36 @@ dotenv.config()
 const BOT_TOKEN = process.env.BOT_TOKEN || 'qwe'
 
 module.exports = async (req: any, res: any) => {
-    class Bot {
-        bot: Telegraf<IBotContext>
-        commands: Command[] = []
-        constructor(private readonly configService: IConfigService){
-            this.bot = new Telegraf<IBotContext>(BOT_TOKEN)
-            this.bot.use(new LocalSession({ database: 'sessions.json'}).middleware())
-        }
-        init() {
-            this.commands = [new GoDotaCommand(this.bot), new MobilizationCommand(this.bot), new StikerDrop(this.bot)]
-            for(const command of this.commands){
-                command.handle()
+    try{
+        class Bot {
+            bot: Telegraf<IBotContext>
+            commands: Command[] = []
+            constructor(private readonly configService: IConfigService){
+                this.bot = new Telegraf<IBotContext>(BOT_TOKEN)
+                this.bot.use(new LocalSession({ database: 'sessions.json'}).middleware())
             }
-            this.bot.launch()
+            init() {
+                this.commands = [new GoDotaCommand(this.bot), new MobilizationCommand(this.bot), new StikerDrop(this.bot)]
+                for(const command of this.commands){
+                    command.handle()
+                }
+                this.bot.launch()
+            }
         }
+        
+        const bot = new Bot(new ConfigService())
+        
+        bot.init()
+        
+        exports.handler = async (event:any) => {
+            console.log("Received an update from Telegram!", event.body)
+            return { statusCode: 200 }
+        };
+    
+        res.send('OK');
     }
-    
-    const bot = new Bot(new ConfigService())
-    
-    bot.init()
-    
-    exports.handler = async (event:any) => {
-        console.log("Received an update from Telegram!", event.body)
-        return { statusCode: 200 }
-    };
-
-    res.send('OK');
+    catch(err) { 
+        console.error('Error sending message');
+        console.log(err);
+    }
 }
